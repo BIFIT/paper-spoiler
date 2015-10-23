24c42a87e30bf4e69feafc86e88f5b7bc7596225
Denis Baskovsky <baskovskiy@bifit.com>
2015-10-23 18:39:10 +0300

    New rotate animation

--- paper-spoiler.js
+++ paper-spoiler.js
@@ -14,10 +14,10 @@ Polymer({
     var $content = this.$$('.content');
 
     if (showed) {
-      this.dropdownIcon = 'expand-less';
+      this.transform(`rotateZ(${0}deg)`, this.$.dropdownIcon);
       $content.classList.add('active');
     } else {
-      this.dropdownIcon = 'expand-more';
+      this.transform(`rotateZ(${-180}deg)`, this.$.dropdownIcon);
       $content.classList.remove('active');
     }
   },
